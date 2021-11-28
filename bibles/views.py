7c0ffb6ff4deefb5276fd3d43fcbd9a5be2db1a8
jjangssyy <yuiyui6258@gmail.com>
2021-11-28 18:41:51 +0900

    modify templates to display bibles from db

--- bibles/views.py
+++ bibles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Script
+from django.utils import timezone
 
 # Create your views here.
 def bible_list(request):
-    return render(request, 'bibles/bible_list.html', {})
+    bibles = Script.objects.order_by('published_date')
+    return render(request, 'bibles/bible_list.html', {'bibles': bibles})
